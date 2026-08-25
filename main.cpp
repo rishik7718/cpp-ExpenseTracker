6ca73e82480c6e09d714e19082696dd80b3c028a
Rishik <rihikkumar7718@gmail.com>
2026-08-25 10:17:48 +0530

    Added Expenses Insights(max,min,avg)

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int choice,i,amount[100];
+	string description[100],category[100];
+	int expensecount=0;
+	do
+	{
+	 cout<<"**********Expense Tracker**********"<<endl;
+	cout<<"1. Add Expense "<<endl;
+	cout<<"2 . Veiw Expense "<<endl;
+	cout<<"3 . Total Expense "<<endl;
+	cout<<"4 . Exit "<<endl;
+	cout<<"Enter Your Choice : ";
+	cin>>choice;
+	system("cls");
+	switch(choice)
+	{
+		case 1 :
+		if(expensecount<100) 
+		{cout<<"Enter The Description : ";
+		cin>>description[expensecount];
+		cout<<"Enter Category : ";
+		cin>>category[expensecount];
+		cout<<"Enter Amount : ";
+		cin>>amount[expensecount];
+		cout<<"Expense Added !!"<<endl<<endl;
+		expensecount++;}
+		else
+		{
+			cout<<"Storage Full !!"<<endl<<endl;
+		}
+		break;
+		
+		case 2 :
+			if(expensecount>0)
+		{for (i=0;i<expensecount;i++)
+		     {cout<<"Description : "<<description[i]<<endl;
+		cout<<"Category : "<<category[i]<<endl;
+		cout<<"Amount : "<<amount[i]<<endl<<endl;}
+	    }
+		else
+		{
+			cout<<"No Expenses Yet !! "<<endl<<endl;
+			}
+		break;
+		
+		case 3 :
+			{
+			int totalexpense=0;
+			for(i=0;i<expensecount;i++)
+			{
+			  cout << i+1<<" . "<< category[i]<<" = " << amount[i] << endl;
+				totalexpense=totalexpense+amount[i];
+			}
+		cout<<"----------------"<<endl;	
+		cout <<"Your Total Expense = "<<totalexpense<<endl<<endl;
+		cout<<"----------------"<<endl;
+		break;	}	
+			
+		case 4 :
+		cout<<"Number Of Expenses Today : "<<expensecount<<endl<<endl;
+		cout<<"Thank You !! "<<endl;
+		break;
+		
+		default :
+		cout<<"Invalid Choice !!"<<endl<<endl;	
+	}
+  }
+  while(choice!=4);
+}
